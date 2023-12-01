01815e411c06aa0bad9c6d12f62997aa27fc6bab
tomascarreira <62406519+tomascarreira@users.noreply.github.com>
2023-12-01 11:50:44 +0000

    Change a name of function to be more correct

--- day1-1.hs
+++ day1-1.hs
@@ -7,12 +7,12 @@ main = do
   print $ sum $ calibrationValues $ lines contents
 
 calibrationValues :: [String] -> [Int]
-calibrationValues  = map (read . sumFirstAndLast . parseLine)
+calibrationValues  = map (read . firstAndLast . parseLine)
 
-sumFirstAndLast :: String -> String
-sumFirstAndLast [a] = [a,a]
-sumFirstAndLast [a, b] = [a,b]
-sumFirstAndLast (a:b:rest) = sumFirstAndLast (a:rest)
+firstAndLast :: String -> String
+firstAndLast [a] = [a,a]
+firstAndLast [a, b] = [a,b]
+firstAndLast (a:b:rest) = firstAndLast (a:rest)
 
 parseLine :: String -> String
 parseLine = filter isDigit
